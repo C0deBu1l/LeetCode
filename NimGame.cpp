8546d54427cc9da7ab465036297190c41310e06b
C0deBu1l <geek0lychee@gmail.com>
2017-04-23 09:56:28 +0800

    Solved Nim Game

--- NimGame.cpp
+++ NimGame.cpp
@@ -7,11 +7,9 @@ class Solution
 public:
   bool CanWinNim(int n)
   {
-    bool nim = false;
-    if(n % 3 == 0){
-      nim = true;
-    }else if(n % 2 == 0){
-      nim = true;
+    bool nim = true;
+    if(0 == n % 4){
+      return false;
     }
 
     return nim;
@@ -21,7 +19,7 @@ public:
 int main(int argc, char **argv)
 {
   Solution nim;
-  bool result = nim.CanWinNim(4);
+  bool result = nim.CanWinNim(7);
   cout << "Nim result is " << result << endl;
 
   return 0;
